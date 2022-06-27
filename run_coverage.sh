6da54c237f45845115795c761068fc9a886d5604
gmpassos <gracilianomp@gmail.com>
2022-06-26 23:38:30 -0300

    Update `run_coverage.sh`

---
+++ run_coverage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dart run test --coverage=./coverage -p chrome
+
+dart pub global run coverage:format_coverage --packages=.dart_tool/package_config.json --report-on=lib --lcov -o ./coverage/lcov.info -i ./coverage
+
+genhtml -o ./coverage/report ./coverage/lcov.info
+open ./coverage/report/index.html
